0f7d0b415ca8eda51680b88339119646cae05633
mbernier <mkbernier@gmail.com>
2017-09-24 20:03:47 -0600

    ordered the users

--- project/api/views.py
+++ project/api/views.py
@@ -20,7 +20,7 @@ def ping_pong():
 @users_blueprint.route('/users', methods=['GET'])
 def get_all_users():
     """Get all users"""
-    users = User.query.all()
+    users = User.query.order_by(User.created_at.desc()).all()
     users_list = []
     for user in users:
         user_object = {
@@ -101,12 +101,12 @@ def get_single_user(user_id):
         return jsonify(response_object), 404
 
 
-@users_blueprint.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
-    users = User.query.order_by(User.created_at.desc()).all()
-    return render_template('index.html', users=users)
+# @users_blueprint.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         email = request.form['email']
+#         db.session.add(User(username=username, email=email))
+#         db.session.commit()
+#     users = User.query.order_by(User.created_at.desc()).all()
+#     return render_template('index.html', users=users)
